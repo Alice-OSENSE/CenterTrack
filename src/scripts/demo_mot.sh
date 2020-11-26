f53c1bc8de566fd12a9bf5ee3fee99f997846efa
Alice-OSENSE <alice.li@osensetech.com>
2020-11-26 17:15:58 +0800

    Add script to run demo

---
+++ src/scripts/demo_mot.sh
@@ -0,0 +1,6 @@
+python3 ../demo.py tracking \
+--load_model "../../models/mot17_fulltrain.pth" \
+--num_class 1 \
+--demo "/home/osense-office/Documents/sample_video/LA_vs_MI_clip.mp4" \
+--save_video \
+--no_pre_img
